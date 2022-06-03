2ed6485f8feb1617d02e515098c25d6513a0c885
kuwooo <3257132998@qq.com>
2022-06-04 00:16:33 +0800

    优化一处代码处理逻辑
    优化一处代码处理逻辑

--- src/isHtml.ts
+++ src/isHtml.ts
@@ -1,7 +1,8 @@
 export const isHtml = (html: string): boolean => {
   try {
     if (typeof html !== 'string') {
-      html = '';
+//       html = '';
+      return false;
     }
     const trimmed = html.replace(/^[ \t\n\r]+/, '');
     return Boolean(trimmed) && (trimmed.substring(0, 1) === '<') && (trimmed.charAt(trimmed.length - 1) === '>');
