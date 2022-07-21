0e2d58cc96662557e4e5fb12b200e1d80f91ca12
宣博文 <1030698842@qq.com>
2022-07-22 00:52:17 +0800

    update complehttp

--- src/completionHttpProtocol.ts
+++ src/completionHttpProtocol.ts
@@ -1,18 +1,21 @@
 const completionHttpProtocol = (data: any) => {
   if (Object.prototype.toString.call(data) === '[object String]') {
     if (
-      data.substr(0, 7).toLowerCase() !== 'http://' &&
-      data.substr(0, 8).toLowerCase() !== 'https://'
+      data.toLowerCase().substr(0, 7).toLowerCase() !== 'http://' &&
+      data.toLowerCase().substr(0, 8).toLowerCase() !== 'https://'
     ) {
       data = `http://${data}`;
     }
   } else if (Object.prototype.toString.call(data) === '[object Object]' && data.hasOwnProperty('url')) {
-    if (
-      data.url.substr(0, 7).toLowerCase() !== 'http://' &&
-      data.url.substr(0, 8).toLowerCase() !== 'https://'
-    ) {
-      data.url = `http://${data.url}`;
+    if(Object.prototype.toString.call(data?.url) === '[object String]'){
+      if (
+        data.url.toLowerCase().substr(0, 7).toLowerCase() !== 'http://' &&
+        data.url.toLowerCase().substr(0, 8).toLowerCase() !== 'https://'
+      ) {
+        data.url = `http://${data.url}`;
+      }
     }
+    
   }
   return data;
 };
