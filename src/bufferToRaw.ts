8b4f1c059598583d7e88b24b8a2fe54254e51b21
宣博文 <1030698842@qq.com>
2022-07-21 22:24:55 +0800

    add bufferToRaw

---
+++ src/bufferToRaw.ts
@@ -0,0 +1,16 @@
+const BufferO = require('buffer/').Buffer
+const zlib = require('zlib');
+
+const bufferToRaw = function (buffer:any, mime:any) {
+    let _inflateBuffer = null;
+    try {
+        _inflateBuffer = zlib.inflateSync(BufferO.from(buffer))
+    } catch (e) {
+        _inflateBuffer = BufferO.from(buffer);
+    }
+    return {
+        buffer: _inflateBuffer,
+        raw: _inflateBuffer.toString(),
+        base64: `data:${mime['mime']};base64,${_inflateBuffer.toString('base64')}`
+    }
+}
