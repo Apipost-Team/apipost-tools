66512218ebf95732563700c7227e7d8038138a35
trqdz <trqdz@hotmail.com>
2023-05-11 18:45:02 +0800

    fix:修复美化js代码时意外执行不安全代码的bug

--- src/beautifyRaw.ts
+++ src/beautifyRaw.ts
@@ -7,17 +7,20 @@ import formatHtml from './formatHtml';
 import isJsonp from './isJsonp';
 import jsonp2Obj from './jsonp2Obj';
 
+
 export const beautifyRaw = (data: any) => {
   let beautifyRaw = '';
   let mode = 'text';
-  if (Object.prototype.toString.call(data) === '[object Object]') {  // 传入数据为对象
+  if (Object.prototype.toString.call(data) === '[object Object]') {
+    // 传入数据为对象
     try {
       beautifyRaw = JSONbig.stringify(data, null, '\t');
     } catch (error) {
       beautifyRaw = JSON.stringify(data, null, '\t');
     }
     mode = 'json';
-  } else if (Object.prototype.toString.call(data) === '[object String]') { // 传入数据为字符串
+  } else if (Object.prototype.toString.call(data) === '[object String]') {
+    // 传入数据为字符串
     if (isJSON(data)) {
       try {
         beautifyRaw = JSONbig.stringify(JSONbig.parse(data), null, '\t');
@@ -39,13 +42,14 @@ export const beautifyRaw = (data: any) => {
         try {
           beautifyRaw = _obj[0] + JSON.stringify(JSON.parse(_obj[1]), null, '\t') + _obj[2];
         } catch (error) {
-          console.log(error, "美化JSONP失败");
+          console.log(error, '美化JSONP失败');
         }
       }
       mode = 'json';
     } else {
       try {
-        eval(data);
+       // eval(data);
+        eval(`(function sandbox(){ ${data} })`)
         mode = 'javascript';
       } catch (error) {
         mode = 'text';
